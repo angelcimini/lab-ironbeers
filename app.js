1e23d788b713cf4eb61e627c06c5573ae80ed932
Nahuel Cimini <nahuel.cimini@gmail.com>
2022-10-07 17:21:27 +0200

    patatabeer

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(__dirname + "/views/partials");
 // ...
 
 // Add the route handlers here:
@@ -21,5 +21,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then((beersArr) => {
+    const data = {
+      beer: beersArr
+    }
+  .catch(()=> console.log("Error patata"))
+    res.render('beers', data);
+  })
+});
+app.get('/randomBeers', (req, res) => {
+  punkAPI.getRandom()
+  .then((randomBeers) => {
+    const data = randomBeers[0]
+    console.log(data)
+    res.render('randomBeers', data);
+    // res.render('randomBeers', randomBeers[0])
+  })
+});
+
+// app.get('/randomBeers', async (req, res) => {
+//   const beersArr = await punkAPI.getRandom()
+//   res.render('randomBeers', beersArr[0])
+// })
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
